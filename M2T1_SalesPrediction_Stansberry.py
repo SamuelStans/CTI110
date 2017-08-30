c0ac705392818e0b79b0514d83b500ab447ac9d7
SamuelStans <31484844+SamuelStans@users.noreply.github.com>
2017-08-30 13:14:37 -0400

    Add files via upload

---
+++ M2T1_SalesPrediction_Stansberry.py
@@ -0,0 +1,11 @@
+
+# CTI-110 
+# M2T1 - Sales Prediction 
+# Name Samuel Stansberry
+# 8/30/17
+#Write a program that projects the total amount of sales and display the corrected amount
+total_sales = float(input('Enter the projected sales: '))
+
+profit = total_sales * 0.23
+
+print ('The profit is $', format(profit, ',.2f'))
